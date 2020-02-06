ba5d234789b93acd3d8d19bbb61119d9ee2da1da
Sheppy <sheppy@atlantishq.de>
2020-02-06 18:04:49 +0100

    code cleanup since 2014

--- backup_manager.py
+++ backup_manager.py
@@ -4,13 +4,14 @@
 import dateutil.parser
 import os
 from datetime import *
-from systemd import journal
+import argparse
 
 ### DEFAULTS ###
-backup_dir   = "/home/backup/"
+EXTENSIONS = [".tar", ".tar.gz", ".tar.gz.gpg"]
+DEBUG      = False
+SYSLOG     = False
+
 backup_name  = "all"
-extensions   = ["tar.gz","tar.gz.gpg"]
-debug        = False
 
 max_daily   = 7
 max_weekly  = 3
@@ -22,76 +23,101 @@ day_of_monthly_backup   = 1
 day_of_weekly_backup    = 1       #monday = 1, sunday = 7
 month_of_yearly_backup  = 1
 
-### Journal Streams ###
-log_debug = journal.stream("BackupRotation-" + str(backup_name),priority=7)
-log_warning = journal.stream("BackupRotation-" + str(backup_name),priority=4)
+delList = dict()
 
 def debug(s):
-    if debug:
-        print("DEBUG: "+str(s))
+    if DEBUG:
+        print(s)
 
-### FUNCTIONS ###
-del_list = dict()
-def func_walk_dir(dir_name):
-    for folder, subs, files in os.walk(dir_name):
-        for f in files:
-            filearg = func_parse_filename(f,dir_name)
-    for k,v in del_list.items():
-            #debug(len(v))
-        if len(v) <= min_total:
-            log_debug.write(str(v[0]) + " not removed because there are less than " + str(min_total) + " backups remaining.")
+def checkDirectory(dirName):
+    '''Check a directory for files'''
+   
+    # mark files for potential deletion #
+    for folder, subs, files in os.walk(dirName):
+        for fname in files:
+            parseFilename(fname, dirName)
+
+    # check and delete if nessesary #
+    for name,entries in delList.items():
+        if len(entries) <= min_total:
+            debug("Not removing '{}' - less than {} backups ({})".format(name, min_total, len(entries)))
             continue
         else:
-            for entry in v:
-                func_delete(*entry)
+            for entry in entries:
+                handleDelete(*entry)
 
-def func_parse_filename(filename,dir_name):
-    underscore_split = filename.split('_')
-    name = underscore_split[0]
+def parseFilename(filename, dirName):
+    '''Check filename for relevant indentifiers'''
+    global delList
+
     try:
-        date = underscore_split[1].split('.')[0]
-        extension = filename[len(name)+len(date)+2:]
-    except IndexError:
-        log_debug.write(filename + " in " + dir_name + " no extension " + "-> file ignored\n")
-        return
-    if extension in extensions:
-            #debug((name,filename,dir_name,date,extension))
-        if name in del_list:
-            del_list[name] += [(filename,dir_name,date,name,extension)]
+        name, dateAndExtensions = filename.split('_')
+        date = dateAndExtensions.split('.')[0]
+        extension = filename[len(name) + len(date) + 1:]
+        if extension in EXTENSIONS:
+            if name in delList:
+                delList[name] += [(filename, dirName, date, name, extension)]
+            else:
+                delList.update({name:[(filename, dirName, date, name, extension)]})
         else:
-            del_list[name] =  [(filename,dir_name,date,name,extension)]
+            debug("Not removing {} {} - extension not whitelisted".format(dirName, filename))
+    except (IndexError, ValueError):
+        debug("Malformed filename {} {} -> ignored".format(dirName, filename))
 
-def func_delete(filename,dir_name,str_logdate,name,ext):
+
+def handleDelete(filename, dir_name, str_logdate, name, ext):
+    '''Check file against given constraint and delete it if nessesary.
+            return: Bool - Information if file has been deleted'''
+
     today = datetime.today()
+
+    # determine date of backup #
     try:
         logdate = dateutil.parser.parse(str_logdate)
     except ValueError:
-        debug(("Value error: "+ filename))
-        log_debug.write(filename + " in "+ dir_name + " unrecognized date format: " + str_logdate + " -> file ignored\n")
+        debug("Path {} {} failed (unrecognized format)".format(dirname, filename))
         return False
-    if ( (today - logdate).days < max_daily ):
-        debug(("daily: " + filename))
+
+    # check constraints #
+    if (today - logdate).days < max_daily:
+        debug("Keeping daily: {}".format(filename))
         return False
-    if ( logdate.isoweekday() == day_of_weekly_backup  and (today - logdate).days / 7 < max_weekly  ):
-        debug(("weekly: " + filename))
+    if logdate.isoweekday() == day_of_weekly_backup  and (today - logdate).days / 7 < max_weekly:
+        debug("Keeping weekly: {}".format(filename))
         return False
-    if ( logdate.day == day_of_monthly_backup and   today.year == logdate.year and today.month - logdate.month  < max_monthly ):
-        debug(("monthly: " + filename))
+    if logdate.day == day_of_monthly_backup and today.year == logdate.year \
+                                            and today.month - logdate.month  < max_monthly:
+        debug("Keeping monthly: {}".format(filename))
         return False
-    if ( logdate.day == day_of_monthly_backup and   logdate.month == month_of_yearly_backup and today.year - logdate.year    < max_yearly  ): 
-        debug(("yearly: "+filename))
+    if logdate.day == day_of_monthly_backup and logdate.month == month_of_yearly_backup \
+                                            and today.year - logdate.year    < max_yearly:
+        debug("Keeping yearly: {}".format(filename))
         return False
+    
+    # remove file #
     try:
         os.remove(os.path.join(dir_name,filename))
+        debug("Deleted: {}".format(filename))
     except Exception as e:
-        log_warning.write(str(e)+"\n")
-        debug(("exception: " + filename + " " + str(e)))
+        debug("{} failed: {}".format(filename, e))
         return False
-    debug(("deleted:" + filename))
+
     return True
 
 ### SCRIPT ###
-if backup_dir != None:
-    func_walk_dir(backup_dir)
-    log_warning.close()
-    log_debug.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Simple python backup manager', 
+                                        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('PATH', nargs='+', help='Paths to check for backups')
+    parser.add_argument('--extensions', '-e', nargs='+', default=EXTENSIONS, 
+                            help='Whitelist for exentions to handle')
+    parser.add_argument('--debug', action='store_const', default=DEBUG, const=True, 
+                            help='Print debugging information')
+
+    args = parser.parse_args()
+
+    EXTENSIONS = args.extensions
+    DEBUG = args.debug
+
+    for backupDir in args.PATH:
+        checkDirectory(backupDir)
